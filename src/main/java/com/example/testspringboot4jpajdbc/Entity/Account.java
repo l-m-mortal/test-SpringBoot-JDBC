30dff359f65b385b5cc1252473e1555127376aa2
l-m-mortal <Lamixjr@icloud.com>
2022-06-15 12:23:31 +0300

    Update1 (fixed annotations for db, corrected sql syntax)

--- src/main/java/com/example/testspringboot4jpajdbc/Entity/Account.java
+++ src/main/java/com/example/testspringboot4jpajdbc/Entity/Account.java
@@ -1,2 +1,74 @@
-package com.example.testspringboot4jpajdbc.Entity;public class Account {
+package com.example.testspringboot4jpajdbc.Entity;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private Integer bill;
+
+    public Account(Long id, String name, String email, Integer bill) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bill = bill;
+    }
+
+    public Account() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getBill() {
+        return bill;
+    }
+
+    public void setBill(Integer bill) {
+        this.bill = bill;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", bill=" + bill +
+                '}';
+    }
 }
--- src/main/java/com/example/testspringboot4jpajdbc/TestSpringBoot4JpaJdbcApplication.java
+++ src/main/java/com/example/testspringboot4jpajdbc/TestSpringBoot4JpaJdbcApplication.java
@@ -1,13 +1,46 @@
 package com.example.testspringboot4jpajdbc;
 
+import com.example.testspringboot4jpajdbc.Entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @SpringBootApplication
-public class TestSpringBoot4JpaJdbcApplication {
+
+public class TestSpringBoot4JpaJdbcApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TestSpringBoot4JpaJdbcApplication.class, args);
     }
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @PostConstruct
+    public void initDB() {
+        jdbcTemplate.update("create table if not exists Account ()");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        jdbcTemplate.execute("INSERT INTO Account (id, name, email, bill) VALUES (1, 'Mike', 'Mike@email.xxx', 4000)");
+        Account accountById = findAccountById(1l);
+        System.out.println(accountById);
+    }
+
+    private Account findAccountById(Long accountId) {
+        String query = "SELECT * FROM Account WHERE  id=%s";
+        Map<String, Object> resultSet = jdbcTemplate.queryForMap(String.format(query, accountId));
+        Account account = new Account();
+        account.setId(accountId);
+        account.setName((String)resultSet.get("name"));
+        account.setEmail((String)resultSet.get("email"));
+        account.setBill((Integer)resultSet.get("bill"));
+        return  account;
+    }
 }
